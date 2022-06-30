b16fa4c4979666eaad804a3808beca212e8455fb
lyfhouyi MagicBook <lyfhouyi@163.com>
2022-06-30 14:58:36 +0800

    houyi

--- assignment/homework5/src/denoiser.cpp
+++ assignment/homework5/src/denoiser.cpp
@@ -46,7 +46,53 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             // TODO: Joint bilateral filter
-            filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            // houyi 2022.6.30
+            // filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            int jXStart = std::max(0,x-kernelRadius);
+            int jXEnd = std::min(width-1,x+kernelRadius);
+            int jYStart = std::max(0,y-kernelRadius);
+            int jYEnd = std::min(height-1,y+kernelRadius);
+            float weights_sum = 0.0;
+            for(int jx = jXStart;jx<=jXEnd;jx++)
+            {
+                for(int jy=jYStart;jy<=jYEnd;jy++)
+                {
+                    // 像素距离权重
+                    float weight_distance = -SqrDistance(frameInfo.m_position(jx,jy),frameInfo.m_position(x,y))/(2*m_sigmaCoord*m_sigmaCoord);
+                    
+                    // 像素颜色权重
+                    float weight_color = -SqrDistance(frameInfo.m_beauty(jx,jy),frameInfo.m_beauty(x,y))/(2*m_sigmaColor*m_sigmaColor);
+                    
+                    // 像素法线权重
+                    float weight_normal = SafeAcos(Dot(frameInfo.m_normal(jx,jy),frameInfo.m_normal(x,y)));
+                    weight_normal = (weight_normal*weight_normal)/(-2*m_sigmaNormal*m_sigmaNormal);
+                    
+                    // 像素深度权重
+                    float weight_plane = 0.0;
+                    float distance = Distance(frameInfo.m_position(jx,jy),frameInfo.m_position(x,y));
+                    if (distance>0)
+                    {
+                        weight_plane=std::max(0.0f, Dot(frameInfo.m_normal(x,y),(frameInfo.m_position(jx,jy)-frameInfo.m_position(x,y))/distance));
+                    }
+                    weight_plane = (weight_plane*weight_plane)/(-2*m_sigmaPlane*m_sigmaPlane);
+                    
+                    // 像素总 权重
+                    float weight = std::exp(weight_distance+weight_color+weight_normal+weight_plane);
+
+                    weights_sum+=weight;
+                    filteredImage(x, y) += frameInfo.m_beauty(jx, jy) * weight;
+                }
+            }
+
+            // 归一化
+            if(weights_sum==0)
+            {
+                filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            }
+            else
+            {
+                filteredImage(x, y) /=weights_sum;
+            }
         }
     }
     return filteredImage;
